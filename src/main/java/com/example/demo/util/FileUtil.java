f041d2ab8fcfb33f5ca0d9f6da75a518502cad6d
huaiching <45527637+huaiching@users.noreply.github.com>
2025-04-24 14:13:26 +0800

    Update FileUtil.java

--- src/main/java/com/example/demo/util/FileUtil.java
+++ src/main/java/com/example/demo/util/FileUtil.java
@@ -1,13 +1,14 @@
 package com.example.demo.util;
 
 import com.deepoove.poi.XWPFTemplate;
-import com.example.demo.dto.ZipFlieDto;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.pdf.PdfCopy;
-import com.itextpdf.text.pdf.PdfImportedPage;
-import com.itextpdf.text.pdf.PdfReader;
+import com.example.demo.dto.utils.ZipFlieDto;
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfCopy;
+import com.lowagie.text.pdf.PdfImportedPage;
+import com.lowagie.text.pdf.PdfReader;
 import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
 import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.jxls.common.Context;
 import org.jxls.util.JxlsHelper;
@@ -15,7 +16,6 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
@@ -24,8 +24,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
@@ -86,25 +86,33 @@ public class FileUtil {
 
     /**
      * PDF 檔案合併 (iText)
-     * @param pdfFlieLisst 要合併的 PDF 清單
+     * @param pdfFlieList 要合併的 PDF 清單
      * @return PDF 資料流
      */
-    public static byte[] mergePDF(List<byte[]> pdfFlieLisst) {
+    public static byte[] mergePDF(List<byte[]> pdfFlieList) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
         try {
-            Document document = new Document();
-            PdfCopy pdfCopy = new PdfCopy(document, outputStream);
+            // 創建 PDF 合併相關變數
+            com.lowagie.text.Document document = new Document();
+            com.lowagie.text.pdf.PdfCopy pdfCopy = new PdfCopy(document, outputStream);
             document.open();
-            // PDF合併
-            for (byte[] pdfFlie : pdfFlieLisst) {
-                PdfReader pdfReader = new PdfReader(pdfFlie);
+            // PDF 合併
+            for (byte[] pdfFlie : pdfFlieList) {
+                // 讀取文件
+                com.lowagie.text.pdf.PdfReader pdfReader = new PdfReader(pdfFlie);
+                // 取得頁數
                 int pdfPages = pdfReader.getNumberOfPages();
-                for (int i = 1; i <= pdfPages; i++) {
-                    PdfImportedPage importedPage = pdfCopy.getImportedPage(pdfReader, i);
+                // 添加至輸出文檔
+                for (int pdfPage = 1; pdfPage <= pdfPages; pdfPage++) {
+                    PdfImportedPage importedPage = pdfCopy.getImportedPage(pdfReader, pdfPage);
                     pdfCopy.addPage(importedPage);
                 }
+                // 關閉 PdfReader
                 pdfReader.close();
             }
+            // 關閉 Document
+            document.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -134,18 +142,83 @@ public class FileUtil {
     }
 
     /**
-     * 檔案格式 使用的 ResponseEntity
-     * @param fileName  檔案名稱
-     * @param fileBytes 檔案資料流
-     * @return 檔案格式 ResponseEntity
+     * 產生 純文字檔 (自動換行) (BIG5)
+     * @param contentList 每行的文字內容
+     * @return 純文字檔 資料流
      */
-    public static ResponseEntity<Resource> responseEntity(String fileName, byte[] fileBytes) throws UnsupportedEncodingException {
-        // 文件打包
-        Resource resource = new ByteArrayResource(fileBytes);
-        // 文件下載
-        HttpHeaders respHeaders = new HttpHeaders();
-        respHeaders.setContentDispositionFormData("attachment", URLEncoder.encode(fileName, "UTF-8"));
+    public static byte[] generateTxtBIG5(List<String> contentList) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        StringBuilder stringBuilder = new StringBuilder();
+        // 寫入內文 (自動換行)
+        for (String content : contentList) {
+            stringBuilder.append(content);
+            stringBuilder.append("\r\n");
+        }
+        try {
+            outputStream.write(stringBuilder.toString().getBytes("BIG5"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return outputStream.toByteArray();
+    }
 
+    /**
+     * 產生 純文字檔 (自動換行) (UTF-8)
+     * @param contentList 每行的文字內容
+     * @return 純文字檔 資料流
+     */
+    public static byte[] generateTxtUTF8(List<String> contentList) {
+        byte[] big5Bytes = generateTxtBIG5(contentList);
+
+        // 將 Big5 byte[] 解碼為 String
+        String decodedString = new String(big5Bytes, Charset.forName("Big5"));
+
+        // 將 String 重新編碼為 UTF-8 byte[]
+        return decodedString.getBytes(StandardCharsets.UTF_8);
+    }
+
+    /**
+     * 文字格式化: 依照 最大字元數，自動補齊 或 擷取
+     * @param text  文字內容
+     * @param maxLength 最大字元數
+     * @return  格式化後的文字
+     */
+    public static String formatTxt(String text, int maxLength) {
+        if (StringUtils.isEmpty(text)) {
+            return "";
+        }
+        // 計算 文字長度: 利用 BIG5 中文 佔 2 byte 的特性
+        Charset big5 = Charset.forName("BIG5");
+        byte[] bytes = text.getBytes(Charset.forName("BIG5"));
+        // 進行 文字的 擷取 與 補齊空格 的作業
+        if (bytes.length > maxLength) {
+            // 截取超過字節的部分
+            return new String(bytes, 0, maxLength, big5);
+        } else {
+            // 補齊空格
+            StringBuilder result = new StringBuilder(text);
+            while (result.toString().getBytes(big5).length < maxLength) {
+                result.append(" ");
+            }
+            return result.toString();
+        }
+    }
+
+
+    /**
+     * 產生 檔案下載的 ResponseEntity
+     * @param fileName 檔案名稱
+     * @param fileByte 檔案資料流
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static ResponseEntity<Resource> responseEntity(String fileName, byte[] fileByte) throws UnsupportedEncodingException {
+        // 文件打包
+        Resource resource = new ByteArrayResource(fileByte);
+        // 文件下载
+        HttpHeaders respHeaders = new HttpHeaders();
+        respHeaders.setContentDispositionFormData("attachment",
+                URLEncoder.encode(fileName, "UTF-8"));
         return ResponseEntity.ok()
                 .headers(respHeaders)
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
